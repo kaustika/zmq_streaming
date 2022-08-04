9493e503a662317552d89398461ca532fe86b914
kaustika <snufkinson@yandex.ru>
2022-08-04 21:43:48 +0300

    implement base server and client

---
+++ src/streamer.py
@@ -0,0 +1,17 @@
+import cv2
+import zmq
+
+
+context = zmq.Context()
+socket = context.socket(zmq.PUB)
+socket.bind("tcp://*:5577")
+
+while True:
+    video = cv2.VideoCapture(0)
+
+    while True:
+        img, frame = video.read()
+        socket.send_pyobj(frame)
+        cv2.imshow("STREAMING", frame)
+        if cv2.waitKey(1) & 0xFF==ord("q"):
+            break
---
+++ src/watcher.py
@@ -0,0 +1,18 @@
+import cv2
+import zmq
+
+
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+
+print("Connecting to webcam stream...")
+socket.connect("tcp://localhost:5577")
+
+# '' - no message filter for subscription
+socket.setsockopt_string(zmq.SUBSCRIBE, '')
+
+while True:
+    frame = socket.recv_pyobj()
+    cv2.imshow("WATCHING", frame)
+    if cv2.waitKey(1) & 0xFF==ord("q"):
+        break
