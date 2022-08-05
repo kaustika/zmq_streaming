6a2f1459f764234c64c2211b646b6f315e4e45c5
kaustika <snufkinson@yandex.ru>
2022-08-05 18:46:26 +0300

    refactor server, add source validation

--- src/streamer.py
+++ src/streamer.py
@@ -1,21 +1,49 @@
 import cv2
 import zmq
+import os.path
+from typing import Union, Any
 
 
-context = zmq.Context()
-socket = context.socket(zmq.PUB)
-socket.bind("tcp://*:5577")
+def set_up_connection(ip: str, port: str) -> zmq.Socket:
+    context = zmq.Context()
+    socket = context.socket(zmq.PUB)
+    socket.bind(f"tcp://{ip}:{port}")
+    return socket
 
-video = cv2.VideoCapture(0)
 
-while True:
-    img, frame = video.read()
-    socket.send_pyobj(frame)
-    cv2.imshow("STREAMING", frame)
-    if cv2.waitKey(1) == ord("q") or\
-            cv2.getWindowProperty("STREAMING", cv2.WND_PROP_VISIBLE) <= 0:
-        break
+def window_is_closed(win_name: str) -> bool:
+    q_was_pressed = cv2.waitKey(1) == ord("q")
+    x_was_pressed = cv2.getWindowProperty(win_name, cv2.WND_PROP_VISIBLE) <= 0
+    return q_was_pressed or x_was_pressed
 
-socket.send_pyobj(None)
-video.release()
-cv2.destroyAllWindows()
+
+def is_valid_source(source: Any) -> bool:
+    if not os.path.isfile(source) or source == 0:
+        return False
+    video = cv2.VideoCapture(source)
+    success, _ = video.read()
+    return bool(success)
+
+
+def stream(source: Union[int, str] = 0) -> None:
+    if is_valid_source(source):
+        video = cv2.VideoCapture(source)
+        while True:
+            _, frame = video.read()
+            socket.send_pyobj(frame)
+            cv2.imshow("STREAMING", frame)
+            if window_is_closed("STREAMING"):
+                break
+        # stream was ended manually, send None as a signal for that
+        socket.send_pyobj(None)
+        video.release()
+        cv2.destroyAllWindows()
+    else:
+        print("Check accessibility of your video source!")
+
+
+if __name__=="__main__":
+    socket = set_up_connection(ip="*", port="5577")
+    stream()
+
+
