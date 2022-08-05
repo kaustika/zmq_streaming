ebe9abdb3acfa9b78d1196b022f612d76e82487f
kaustika <snufkinson@yandex.ru>
2022-08-05 19:05:39 +0300

    fix logic in source validation

--- src/streamer.py
+++ src/streamer.py
@@ -2,23 +2,18 @@ import cv2
 import zmq
 import os.path
 from typing import Union, Any
+from util import is_closed_window
 
 
-def set_up_connection(ip: str, port: str) -> zmq.Socket:
+def set_up_server_socket(ip: str, port: str) -> zmq.Socket:
     context = zmq.Context()
     socket = context.socket(zmq.PUB)
     socket.bind(f"tcp://{ip}:{port}")
     return socket
 
 
-def is_closed_window(win_name: str) -> bool:
-    is_pressed_q = cv2.waitKey(1) == ord("q")
-    is_pressed_x = cv2.getWindowProperty(win_name, cv2.WND_PROP_VISIBLE) <= 0
-    return is_pressed_q or is_pressed_x
-
-
 def is_valid_source(source: Any) -> bool:
-    if not os.path.isfile(source) or source == 0:
+    if source != 0 and not os.path.isfile(source):
         return False
     video = cv2.VideoCapture(source)
     success, _ = video.read()
@@ -43,7 +38,7 @@ def stream(source: Union[int, str] = 0) -> None:
 
 
 if __name__=="__main__":
-    socket = set_up_connection(ip="*", port="5577")
+    socket = set_up_server_socket(ip="*", port="5577")
     stream()
 
 
