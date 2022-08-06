e874b1154aac16643c309af26574a01a074d2d10
kaustika <snufkinson@yandex.ru>
2022-08-06 15:15:17 +0300

    touch docs of util funcs

--- src/util.py
+++ src/util.py
@@ -8,7 +8,7 @@ def is_closed_window(win_name: str) -> bool:
     Checks if any signal to close the window was received (letter q in focus
     on the window was pressed or the cross(X) was clicked on).
     :param win_name: name of the window to ckeck;
-    :return:
+    :return: True if closed, False if opened.
     """
     is_pressed_q = cv2.waitKey(1) == ord("q")
     is_pressed_x = cv2.getWindowProperty(win_name, cv2.WND_PROP_VISIBLE) <= 0
@@ -21,7 +21,7 @@ def is_valid_source(source: Any) -> bool:
     be either 0 or <valid_video_path> and not cause problems while reading
     the stream.
     :param source: video source to be checked;
-    :return:
+    :return: True if valid, False if invalid.
     """
     if source != 0 and not os.path.isfile(source):
         return False
